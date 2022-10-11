0fd94b434985721621b1ae74eb3db19c5da5c374
mattis - Helife <mattis.dalleau@epitech.eu>
2022-10-11 09:37:34 +0400

    Starting rooms management

--- Server/main.cpp
+++ Server/main.cpp
@@ -5,107 +5,85 @@
 
 using namespace rtype::net;
 
-/*
+#define RTYPE_PLAYER_COUNT 4
+
 class Room {
 private:
-    std::vector<int> _connected_clients;
-    server& _server;
+    uint8_t _hostID = 0;
+
+    std::array<bool, RTYPE_PLAYER_COUNT> _connected_players = {true, false, false, false};
+    std::unordered_map<uint8_t, int> _client_to_index;
+
+    rtype::net::server& _server;
 
 public:
-    Room(server& server) : _server(server) {}
-
-    void add_client(unsigned int id) {
-        spdlog::info("Room: Client {} connected adding to room", id);
-
-        auto conn1 = ConnectionMessage(id);
-        auto new_client = _server.get_client(id);
-
-        for (const auto& client : _connected_clients) {
-            auto conn2 = ConnectionMessage(client);
-            _server.get_client(client)->send_main(conn1);
-            new_client->send_main(conn2);
-        }
-        _connected_clients.push_back(id);
-    }
-
-    void remove_client(unsigned int id) {
-        spdlog::info("Room: Client {} disconnected removing from room", id);
-        std::erase_if(_connected_clients,
-            [id](unsigned int i) { return i == id; });
-        auto conn = DisconnectMessage(id);
-        for (const auto& client : _connected_clients) {
-            _server.get_client(client)->send_main(conn);
-        }
-    }
-
-    bool has_client(unsigned int id) {
-        return std::find(_connected_clients.begin(), _connected_clients.end(), id)
-                != _connected_clients.end();
-    }
-
-    void send_to_all_by_main(rtype::net::IMessage& message,
-                            const std::vector<int>& except = {})
+    Room(rtype::net::server& server, uint16_t client)
+        : _server(server)
     {
-        for (auto& client : _connected_clients) {
-            const bool can_send = std::find(except.begin(), except.end(), client) == except.end();
-            if (can_send)
-                _server.get_client(client)->send_main(message);
+        _client_to_index[client] = 0;
+    }
+
+    ~Room() = default;
+
+    uint8_t getHostID() const { return _hostID; }
+    void setHostID(uint8_t id) { _hostID = id; }
+
+    uint8_t addPlayer(uint16_t client) {
+        for (uint8_t i = 0; i < RTYPE_PLAYER_COUNT; i++) {
+            if (!_connected_players[i]) {
+                _connected_players[i] = true;
+                _client_to_index[client] = i;
+                return i;
+            }
         }
+        return RTYPE_INVALID_PLAYER_ID;
     }
 
-    void send_to_all_by_feed(rtype::net::IMessage& message,
-                            const std::vector<int>& except = {})
-    {
-        for (auto& client : _connected_clients) {
-            const bool can_send = std::find(except.begin(), except.end(), client) == except.end();
-            if (can_send)
-                _server.get_client(client)->send_feed(message);
-        }
-    }
-};
+    void removePlayer(uint16_t client) {
+        auto index = _client_to_index[client];
 
-void handle_main_message(Room& room, server::event& event)
-{
-    spdlog::info("Server: Received message from client {} of code {}",
-        event.client->id(), (int)event.message->code());
-    if (event.message->code() == message_code::CONN_INIT) {
-        room.add_client(event.client->id());
-    }
-}
-
-void handle_feed_message(Room& room, server::event& event)
-{
-    spdlog::info("FeedMessage: {}", (int)event.message->code());
-    if (event.message->code() == message_code::SYNC_MSG) {
-        spdlog::info("Server: Client {} sent a sync message", event.client->id());
-        room.send_to_all_by_feed(*event.message->to_msg(), {event.client->id()});
-    }
-}
-
-int main()
-{
-    server server(4242, 4243, true);
-    Room room(server);
-
-    while (true) {
-        server::event event;
-        if (server.poll(event)) {
-            spdlog::info("Server: Event received: {}", event.type);
-            if (event.type == server::event_type::FeedMessage) {
-                handle_feed_message(room, event);
-            } else if (event.type == server::event_type::MainMessage) {
-                handle_main_message(room, event);
-            } else if (event.type == server::event_type::Disconnect) {
-                if (room.has_client(event.client->id())) {
-                    room.remove_client(event.client->id());
+        if (index == _hostID) {
+            _hostID = RTYPE_INVALID_PLAYER_ID;
+            for (uint8_t i = 0; i < RTYPE_PLAYER_COUNT; i++) {
+                if (_connected_players[i]) {
+                    _hostID = i;
+                    break;
                 }
             }
         }
+        _connected_players[_client_to_index[client]] = false;
+        _client_to_index.erase(client);
     }
-    return 0;
-}
 
-*/
+    bool clientIsInRoom(uint16_t client) const {
+        return _client_to_index.find(client) != _client_to_index.end();
+    }
+
+    bool isFull() const {
+        return std::all_of(_connected_players.begin(),
+                            _connected_players.end(),
+                            [](bool b) { return b; });
+    }
+
+    bool isEmpty() const {
+        return _hostID == RTYPE_INVALID_PLAYER_ID;
+    }
+};
+
+class RoomManager {
+private:
+    std::unordered_map<std::string, Room> _rooms;
+public:
+    RoomManager() = default;
+    ~RoomManager() = default;
+
+    std::string newRoom(uint16_t client) {
+        std::string roomID = 
+        _rooms[roomID] = Room(_server, client);
+        return roomID;
+    }
+
+};
 
 int main()
 {

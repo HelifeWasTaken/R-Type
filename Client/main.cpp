f6732a857d4f0c7fb87d142ed3db2756be52bd42
mattis - Helife <mattis.dalleau@epitech.eu>
2022-10-20 17:34:55 +0400

    Added register system of the game

--- Client/main.cpp
+++ Client/main.cpp
@@ -2,6 +2,28 @@
 #include "ClientScenes.hpp"
 #include <iostream>
 
+#include "Enemies.hpp"
+
 Game g_game;
 
-PAA_UNSAFE_PROGRAM_START(client_connect, "../Resources.conf");
+PAA_SCENE(ecs)
+{
+
+    PAA_START(ecs)
+    {
+        PAA_REGISTER_COMPONENTS(Enemy, Bullet);
+        PAA_REGISTER_SYSTEM(
+            [](hl::silva::registry& r) {
+                for (const auto&& [_, b] : r.view<Bullet>())
+                    b->update();
+                for (const auto&& [_, e] : r.view<Enemy>())
+                    e->update();
+            }
+        );
+
+        PAA_SET_SCENE(client_connect);
+    }
+
+}
+
+PAA_UNSAFE_PROGRAM_START(ecs, "../Resources.conf");

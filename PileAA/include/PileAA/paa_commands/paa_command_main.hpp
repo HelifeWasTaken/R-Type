8b8778d58b01b7f72ec8e62292f81174a1201f78
mattis - Helife <mattis.dalleau@epitech.eu>
2022-10-25 18:11:20 +0400

    Added sprites animations for each players

--- PileAA/include/PileAA/paa_commands/paa_command_main.hpp
+++ PileAA/include/PileAA/paa_commands/paa_command_main.hpp
@@ -4,9 +4,11 @@
 #include "paa_getters.hpp"
 
 #include <spdlog/spdlog.h>
+#include <filesystem>
 
-#define PAA_PROGRAM_START_CONTENT(baseScene, resources_file)                   \
+#define PAA_PROGRAM_START_CONTENT(binary_path, baseScene, resources_file)      \
     {                                                                          \
+        std::filesystem::current_path(std::filesystem::path(binary_path).parent_path()); \
         paa::setup_paa_system(resources_file);                                 \
         PAA_SET_SCENE(baseScene);                                              \
         bool res = PAA_APP.run();                                              \
@@ -17,20 +19,22 @@
  * @brief Main loop of the game
  */
 #define PAA_PROGRAM_START(baseScene, resources_file)                           \
-    int main()                                                                 \
+    int main(int argc, char **argv)                                                                 \
     {                                                                          \
-            try {                                                              \
-                PAA_PROGRAM_START_CONTENT(baseScene, resources_file);          \
-                return 0;                                                      \
-            } catch (const paa::AABaseError& e) {                              \
-                spdlog::critical("paa::AABaseError: Error: {}", e.what());     \
-            } catch (const std::exception& e) {                                \
-                spdlog::critical("std::exception: Error: {}", e.what());       \
-            } catch (...) {                                                    \
-                spdlog::critical("Unknown error");                             \
-            }                                                                  \
+        try {                                                              \
+            PAA_PROGRAM_START_CONTENT(argv[0], baseScene, resources_file);          \
+            return 0;                                                      \
+        } catch (const paa::AABaseError& e) {                              \
+            spdlog::critical("paa::AABaseError: Error: {}", e.what());     \
+        } catch (const std::exception& e) {                                \
+            spdlog::critical("std::exception: Error: {}", e.what());       \
+        } catch (...) {                                                    \
+            spdlog::critical("Unknown error");                             \
+        }                                                                  \
         return 1;                                                              \
     }
 
 #define PAA_UNSAFE_PROGRAM_START(baseScene, resources_file)                    \
-    int main() { PAA_PROGRAM_START_CONTENT(baseScene, resources_file); }
+    int main(int argc, char **argv) { \
+        PAA_PROGRAM_START_CONTENT(argv[0], baseScene, resources_file); \
+    }
